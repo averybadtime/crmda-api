675bb3f679532ed21e4d9863ccf21d1c5266cf63
Luis Carlos Charris Molina <luis.carlos.charris@gmail.com>
2018-10-19 11:19:08 -0500

    Reporte Ok

--- controllers/reports.js
+++ controllers/reports.js
@@ -4,28 +4,79 @@ const customersRef = admin.database().ref("/customers")
 async function getObjectByRTMLocation(req, res) {
     const start = req.body.start
     const end = req.body.end
-    let ObjectByRtmLocation2018_2 = {}
+		let ObjectByRtmLocation2018_2 = {}
+		let ObjectByRtmLocationSource = {}
     let dataSet = []
     const result2018 = await customersRef.orderByChild("fechaRtm2018Unix").startAt(start).endAt(end).once("value").then(snapshot => {
         const data = snapshot.val()
-        console.log('data2018', data)
+        //console.log('data2018', data)
         if (data !== null) {
             const array = Object.values(snapshot.val())
-            const arrayData = groupArray(array, "location", "rtm")
-            let arrayObj = {}
+						const arrayData = groupArray(array, "location", "rtm")
+						let arrayDataSource = groupArray(array, "sourceKey", "location")
+
+						ObjectByRtmLocationSource = arrayDataSource
+
+						for (let locationKey in arrayDataSource){
+
+							
+							
+							for (let sourceKey in arrayDataSource[locationKey]){
+								ObjectByRtmLocationSource[locationKey][sourceKey] = arrayDataSource[locationKey][sourceKey].length
+								//console.log(locationKey,sourceKey,arrayDataSource[locationKey][sourceKey].length);
+								
+							}
+						}
+
+					//	console.log(ObjectByRtmLocationSource);
+						
+						
+						let arrayObj = {}
+						
             for (const key in arrayData) {
+
+
+							let rtmTalleres2018 = 0
+							let rtmTalleresNuevos = 0
+
+
+							arrayData[key].customer_new.forEach(element => {
+								if(element.garageKey){
+									rtmTalleresNuevos ++
+									//console.log(key,'SI existente', rtmTalleresNuevos);
+								}else{
+									//console.log(key,'No taller, existente');									
+								}
+							});
+
+							arrayData[key].customer_exist.forEach(element => {								
+								if(element.garageKey){
+									rtmTalleres2018 ++								
+								}else{
+									//console.log(key,'No taller, en new');									
+								}								
+							});
+
+							
+
+							
+							
                 const rtm2018 = arrayData[key].customer_exist ? arrayData[key].customer_exist.length : 0
                 const rtmNuevas = arrayData[key].customer_new ? arrayData[key].customer_new.length : 0
-                const rtm2017 = 0
+								const rtm2017 = 0
+							
                 arrayObj[key] = {
                     rtm2017,
                     rtm2018,
-                    rtmNuevas
+										rtmNuevas,
+										rtmTalleres2018,
+										rtmTalleresNuevos,
+										
                 }
             }
             ObjectByRtmLocation2018_2 = arrayObj
-            console.log('p', arrayObj);
-            console.log('objectByRtm', ObjectByRtmLocation2018_2)
+           //console.log('GroupArraySourceKey', arrayDataSource);
+           // console.log('objectByRtm', ObjectByRtmLocation2018_2)
         }
     }).catch(err => {
         console.error(err)
@@ -35,32 +86,34 @@ async function getObjectByRTMLocation(req, res) {
     })
     let start2017 = req.body.start2017
     let end2017 = req.body.end2017
-    console.log('fechas 2017', start2017, end2017)
+    //console.log('fechas 2017', start2017, end2017)
     const resultLastYear = await customersRef.orderByChild('fechaRtmBDUnix').startAt(start2017).endAt(end2017).once('value').then(snapshot => {
         const data = snapshot.val()
-        console.log('data2017', data)
+        //console.log('data2017', data)
         if (data != null) {
             const array = Object.values(data)
             let ObjArray = groupArray(array, 'location')
             for (const key in ObjArray) {
                 if (!ObjectByRtmLocation2018_2[key]) {
-                    console.log('Creamos el rtm2017');
+                    //console.log('Creamos el rtm2017');
                     ObjectByRtmLocation2018_2[key] = {
                         rtm2017: 0,
                         rtm2018: 0,
                         rtmNuevas: 0
                     }
                 }
-                console.log('fechaRtmBDUnix', key, ObjectByRtmLocation2018_2[key]);
+                //console.log('fechaRtmBDUnix', key, ObjectByRtmLocation2018_2[key]);
                 ObjectByRtmLocation2018_2[key].rtm2017 = ObjArray[key].length
                 dataSet.push(ObjectByRtmLocation2018_2[key].rtm2018)
-                console.log('object2', ObjectByRtmLocation2018_2)
+                //console.log('object2', ObjectByRtmLocation2018_2)
             }
         }
-        console.log('dataset', dataSet);
+        //console.log('dataset', dataSet);
         return res.status(200).json({
             results: ObjectByRtmLocation2018_2,
-            dataSet
+						dataSet,
+						localhost: 'OK',
+						ObjectByRtmLocationSource
         })
     })
     setTimeout(() => {
@@ -110,12 +163,15 @@ function getRTMPerdidas(req, res) {
                     totalAccionRtmPerdida += rtmPerdidasByLocation
                 }
             }
-            groupArrayNoRtm = groupArray(arrayNoRtm, "motivo")
+						groupArrayNoRtm = groupArray(arrayNoRtm, "motivo")
+						//const groupArrayNoRtmSede = groupArray(arrayNoRtm, "motivo","sede")
+
             return res.status(200).json({
                 results: ObjectByAccionRtmPerdidas,
                 total: totalAccionRtmPerdida,
                 groupArrayNoRtm,
-                ArrayNoRtmByLocation
+								ArrayNoRtmByLocation,
+								//groupArrayNoRtmSede
             })
         }
         return res.status(200).json({
